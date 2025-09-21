361a5ffcbb83edc128c4abc7c3706c5ef154f1f6
MateoeowManDog <painadler@gmail.com>
2025-09-21 22:20:17 +0800

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from js import document
+from pyscript import document
 
-def show_result(event=None):
-    try:
-        s1 = float(document.getElementById("score1").value)
-        s2 = float(document.getElementById("score2").value)
-        if not (0 <= s1 <= 100 and 0 <= s2 <= 100):
-            raise ValueError
-        avg = (s1 + s2) / 2
-        msg = f"Average Score: {avg:.2f} {'✅ Passed' if avg >= 75 else '❌ Failed'}"
-    except:
-        msg = "Enter valid numbers (0-100)."
+def find_average(event=None):
+    s1 = int(document.getElementById("score1").value or 0)
+    s2 = int(document.getElementById("score2").value or 0)
+
+    avg = (s1 + s2) / 2
+    msg = f"Average: {avg:.2f} ✅ Passed" if avg >= 75 else f"Average: {avg:.2f} ❌ Failed"
+
     document.getElementById("result").innerHTML = msg
